825e31460e220b125e920a8e76388c4f9b2ad05e
ferna <fernandodl15@outlook.com>
2025-04-26 15:58:37 -0600

    ejercicio

---
+++ build/src/ejercicio.js
@@ -0,0 +1,10 @@
+"use strict";
+/*
+Declaren al menos tres variables (nombre, edad, activo) usando los tipos correctos
+imprime un mensaje concatenando esas variables
+*/
+function variables(n, e, a) {
+    console.log(`Hola ${n}, tienes ${e} años y tu estado es ${a}`);
+}
+variables("Fernando", 25, true);
+//# sourceMappingURL=ejercicio.js.map
---
+++ build/src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function sumar(a, b) {
+    return a + b;
+}
+console.log(sumar(2, 3));
+//# sourceMappingURL=index.js.map

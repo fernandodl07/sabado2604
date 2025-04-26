d7156e927a300a79ab6723b3f59bc37eb09bc976
ferna <fernandodl15@outlook.com>
2025-04-26 15:51:09 -0600

    ejercicio

---
+++ src/ejercicio.ts
@@ -0,0 +1,9 @@
+/*
+Declaren al menos tres variables (nombre, edad, activo) usando los tipos correctos
+imprime un mensaje concatenando esas variables
+*/
+
+function variables(n:string, e: number, a: boolean) {
+  console.log(`Hola ${n}, tienes ${e} años y tu estado es ${a}`);
+}
+variables("Fernando", 25, true);
--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ function sumar(a, b) {
 console.log(sumar(2, 3));
 console.log(sumar(5, '4'));
 console.log(sumar('Hola','amigos'));
-console.log(sumar('h','a')) // 9
+console.log(sumar('h','a'))
